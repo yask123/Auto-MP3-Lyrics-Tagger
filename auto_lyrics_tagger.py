afffa9e9edb6f6f6b0a6e217f38debd4e5cea335
Yask Srivastava <yask123@gmail.com>
2014-10-29 02:47:39 +0530

    Added alpha version
    Alpha version

---
+++ auto_lyrics_tagger.py
@@ -0,0 +1,59 @@
+import glob
+import eyed3
+import urllib2
+from bs4 import BeautifulSoup
+
+song_name= glob.glob("*.mp3")
+
+for name in song_name:
+	try:
+
+		print ((name))
+		str = name
+		str = unicode(str, errors='replace')
+		name = str.encode('utf8')
+
+		
+		audiofile = eyed3.load((name))
+		print('Test')
+		name= name.replace('.mp3','')
+		print('Applying to '+name)
+		name = name +' lyrics'
+		name  = name.replace(' ','+')
+
+		url = 'http://www.google.com/search?q='+name
+		print (url)
+		req = urllib2.Request(url, headers={'User-Agent' : "foobar"})
+
+		response = urllib2.urlopen(req)
+		str = response.read()
+		str = unicode(str, errors='replace')
+
+		#print(str.encode('utf8'))
+
+		result = str.encode('utf8')
+
+		link_start=result.find('http://www.metrolyrics.com')
+		link_end=result.find('html',link_start+1)
+		#print(result[link_start:link_start+57])
+
+
+		link = result[link_start:link_end+4]
+		print (link)
+		lyrics_html = urllib2.urlopen(link).read()
+		soup = BeautifulSoup(lyrics_html)
+		raw_lyrics= (soup.findAll('p', attrs={'class' : 'verse'}))
+		paras=[]
+		test1=unicode.join(u'\n',map(unicode,raw_lyrics))
+
+		test1= (test1.replace('<p class="verse">','\n'))
+		test1= (test1.replace('<br/>',' '))
+		test1 = test1.replace('</p>',' ')
+		print (test1)
+		audiofile.tag.lyrics.set(u''+test1 )
+		print('lyrics Added! ')
+		with open (name.replace('+',' ')+'.txt','a') as f:
+			f.write(test1)
+	except :
+		print ('An error occured for '+name)		
+
